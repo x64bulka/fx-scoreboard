e590f9f5fa73950e741a6ff312bf4cad1b4cc417
x64bulka <111202920+x64bulka@users.noreply.github.com>
2022-08-14 16:14:29 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+author 'bulka'
+game 'gta5'
+description 'fx-scoreboard'
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+}
